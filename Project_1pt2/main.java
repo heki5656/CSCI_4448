26e1cac3d1f933b7a395e0011c8b3755b2910d7a
heki5656 <70242959+heki5656@users.noreply.github.com>
2021-09-08 10:20:42 -0600

    Update main.java

--- Project_1pt2/main.java
+++ Project_1pt2/main.java
@@ -5,10 +5,11 @@ public class main{
     public static void main(String args[]){
         //Read();
         Sorter obj = new Sorter();
-        //call method
+        //call method for the Sorter class
         //System.out.println("hiiiii"); this was used to test if main ran
         obj.read();
 
+        //call to the method in the Decoder class
         Decoder obj1 = new Decoder();
         obj1.decode();
 
