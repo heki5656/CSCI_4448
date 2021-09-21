887c8d2e7990b2a61c1826678630a6f52ac0bd27
heki5656 <70242959+heki5656@users.noreply.github.com>
2021-09-21 17:00:07 -0600

    Create Main.java

---
+++ Project_2pt2/Main.java
@@ -0,0 +1,9 @@
+
+public class Main{
+
+    public static void main(String[] args){
+        //create the game objects of each category
+        Store storeObj = new Store();
+        storeObj.dayStimulation();
+    }
+}
