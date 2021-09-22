4cccd53464afbc7ad581db55a8a3a8e125b9e845
heki5656 <70242959+heki5656@users.noreply.github.com>
2021-09-22 16:17:13 -0600

    Update Main.java

--- Project_2pt2/Main.java
+++ Project_2pt2/Main.java
@@ -3,7 +3,9 @@ public class Main{
 
     public static void main(String[] args){
         //create the game objects of each category
+        //instantiates the store object to run the simulation 
         Store storeObj = new Store();
         storeObj.dayStimulation();
+        //this method just calls the method that runs all the other methods and classes to run a 30 day simulation of the game store 
     }
 }
