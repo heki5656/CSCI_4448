ca086caf68c7d931448e848d2d68386ab63b001f
ShawnJaffe <stja9166@colorado.edu>
2021-10-15 15:35:31 -0600

    fixed factory files

--- Project4/src/com/OOAD/Customer.java
+++ Project4/src/com/OOAD/Customer.java
@@ -1,10 +1,45 @@
 package com.OOAD;
 
 public class Customer {
-    String type;
 
-    public Customer(String type){
-        this.type = type;
+    public Customer(){
 
+    }
+}
+
+class FamilyGamer extends Customer{
+
+    public FamilyGamer() {
+        super();
+        System.out.println("Family gamer");
+    }
+}
+
+class KidGamer extends Customer{
+    public KidGamer() {
+        super();
+        System.out.println("Kid gamer");
+    }
+}
+
+class CardGamer extends Customer{
+    public CardGamer() {
+        super();
+        System.out.println("Card gamer");
+    }
+  
+}
+
+class BoardGamer extends Customer{
+    public BoardGamer() {
+        super();
+        System.out.println("Board gamer");
+    }
+}
+
+class CookieMonster extends Customer{
+    public CookieMonster() {
+        super();
+        System.out.println("Cookie Monster");
     }
 }
--- Project4/src/com/OOAD/CustomerFactory.java
+++ Project4/src/com/OOAD/CustomerFactory.java
@@ -4,22 +4,22 @@ public class CustomerFactory{
     public Customer createCustomer(String type){
         Customer customer = null;
         if (type.equals("Family Gamer")) {
-            customer = new FamilyGamer;
+            customer = new FamilyGamer();
         }
         else if (type.equals("Kid Gamer")) {
-            customer = new KidGamer;
+            customer = new KidGamer();
         }
 
         else if (type.equals("Card Gamer")) {
-            customer = new CardGamer;
+            customer = new CardGamer();
         }
 
         else if (type.equals("Board Gamer")) {
-            customer = new BoardGamer;
+            customer = new BoardGamer();
         }
 
         else if (type.equals("Cookie Monster")) {
-            customer = new CookieMonster;
+            customer = new CookieMonster();
         }
 
         return customer;
