7bd0ae7949d96f76136969851d5cfeda03565eaf
ShawnJaffe <70289058+ShawnJaffe@users.noreply.github.com>
2021-10-16 15:12:19 -0600

    Adding robbing to simulator

--- Project4/src/com/OOAD/Simulator.java
+++ Project4/src/com/OOAD/Simulator.java
@@ -6,10 +6,42 @@ public class Simulator {
         //initialize and instantiate the store for the simulation
         Store store = new Store();
 
+        //create a boolean to see if the store was robbed
+        boolean robbed = false;
+
         //run for n days
         for (int day = 1; day <= days; day++) {
             System.out.println("--- Simulation starting Day "+day);
-            store.startADay(day);
+
+            //check if it's day 2, to see if store got robbed after closing on day
+            if (day == 2){
+
+                //chance of being robbed
+                double robbedChance = Utility.rndFromRange(1,100);
+
+                if (robbedChance <= 30){
+                    System.out.println("THE STORE WAS ROBBED!");
+                    robbed = true;
+                }
+
+            
+
+                //check if store was robbed and if so close store for day
+                if (robbed == true){
+                    System.out.println("Store is closed for the day to restock.");
+                }
+
+                //if the store wasn't robbed then open then start the day
+                else{
+                    store.startADay(day);
+                }
+            }
+
+            else{
+                store.startADay(day);
+            }
+
+
         }
 
         //display summary
