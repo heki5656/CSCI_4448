1a8cf03e067b0c741b2a393f3b4199aea0a2aa83
eljay <64116056+eljay@users.noreply.github.com>
2021-10-18 14:45:23 -0600

    Update Utility.java

--- Project4/src/com/OOAD/Utility.java
+++ Project4/src/com/OOAD/Utility.java
@@ -1,10 +1,15 @@
 package com.OOAD;
 
 import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
 
 public interface Utility {
 
-    // making this utility function that can be used by saying Utility.rndFromRange(min,max)
+    Store store = new com.OOAD.Store();
+
+    // making this utility function that can be used by saying com.OOAD.Utility.rndFromRange(min,max)
     // https://www.baeldung.com/java-generating-random-numbers-in-range
     static int rndFromRange(int min, int max) {
         //returns a uniform inclusive random number from a given min and max range
@@ -18,4 +23,34 @@ public interface Utility {
         return formatter.format(value);
     }
 
+    static String selectName(int day) {
+        return store.customers[(day-1) % store.customers.length];
+
+
+//        int len = store.customers.length;
+//
+//        String[] b = new String[(2 * len)];
+//
+//        for (int i = 0; i < len; i++) {
+//            b[i] = b[len + i] = store.customers[i];
+//        }
+//
+//        for (int i = 0; i < b.length; i++) {
+//            String searchedValue = b[i];
+//            ArrayList<String> stringList = new ArrayList<String>(Arrays.asList(store.previousCustomers));
+//
+//            if (!stringList.contains(searchedValue)) {
+//                store.previousCustomers[i] = searchedValue;
+//                return store.previousCustomers[i];
+//            }
+//            else{
+//                store.previousCustomers = new String[]{"", "", "", "", "", "", "", "", "", ""};
+//                store.previousCustomers[i] = searchedValue;
+//                return store.previousCustomers[i];
+//            }
+//        }
+//        return "";
+    }
+
 }
+
