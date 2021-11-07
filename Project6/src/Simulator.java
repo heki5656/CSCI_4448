907d8627fa3a470c2ef7a6c1f1d3c10712c956ab
ShawnJaffe <stja9166@colorado.edu>
2021-11-06 23:20:53 -0600

    initial files

--- Project6/src/Simulator.java
+++ Project6/src/Simulator.java
@@ -2,14 +2,18 @@ package Project6.src;
 
 public class Simulator {
     public void run(int days) {
-        //initialize and instantiate the store for the simulation
-        //Store store = new Store();
 
         //run for n days
         for (int day = 1; day <= days; day++) {
 
-            System.out.println("--- Simulation starting Day " + day);
-            System.out.println("Test")
+            System.out.println("--- Starting Day " + day);
+
+            //check if first day
+            if (day == 1){
+                System.out.println("This is your first day.");
+            }
+
+
         }
     }
 }
