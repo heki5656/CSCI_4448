eef82564398fdfdbfe356c9725deb91f6ac6a4e0
ShawnJaffe <stja9166@colorado.edu>
2021-11-06 23:13:30 -0600

    initial files

---
+++ Project6/src/Simulator.java
@@ -0,0 +1,14 @@
+package Project6.src;
+
+public class Simulator {
+    public void run(int days) {
+        //initialize and instantiate the store for the simulation
+        //Store store = new Store();
+
+        //run for n days
+        for (int day = 1; day <= days; day++) {
+
+            System.out.println("--- Simulation starting Day " + day);
+        }
+    }
+}
