5d503973634acbd859a276701517f61ecae8487f
ShawnJaffe <stja9166@colorado.edu>
2021-11-11 02:31:11 -0700

    added seeds

--- Project6/src/Farm.java
+++ Project6/src/Farm.java
@@ -1,16 +1,29 @@
 package Project6.src;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Farm {
     String map = "[.............]"; //TODO NEED TO IMPLEMENT MAP
 
     Observer observer; //creating mayor observer object
 
+
+    //TODO ALL COSTS, PRICES, AND STATUS NEED TO BE CHANGED FOR SEEDS
+    //instantiate the seed objects
+    Seed carrot = new Seed("carrot", 20, 30, 0/10);
+    Seed turnip = new Seed("turnip", 20, 30, 0/10);
+    Seed onion = new Seed("onion", 20, 30, 0/10);
+    Seed pumpkin = new Seed("pumpkin", 20, 30, 0/10);
+    Seed potato = new Seed("potato", 20, 30, 0/10);
+
+    
+
     public void Farm(){
-        //create the mayor (observer)
-        mayor = new Announcer("Mayor");
+        //create the mayor (observer) ****COMMENTED OUT TO RUN ****
+        //mayor = new Announcer("Mayor"); ****COMMENTED OUT TO RUN ****
         SingletonMayor singletonMayor = null;
-        mayor = singletonMayor.getMayor();
+        //mayor = singletonMayor.getMayor(); ****COMMENTED OUT TO RUN ****
+
     }
 
     //set up the players farm
@@ -75,6 +88,161 @@ public class Farm {
 
     }
 
+    public int plazaOptions(){
+        //TODO NEED TO IMPLEMENT WEATHER INFLUENCE 
+
+        //create a Scanner object
+        Scanner myObj = new Scanner(System.in);
+
+        //display the available plaza choices
+        System.out.println("Plaza actions (please enter the # of the action you would like to select:)");
+        System.out.println("1. Visit the crop shop");
+        System.out.println("2. Visit the seed shop");
+        System.out.println("3. Visit the general shop");
+
+        //store the players choice
+        int choice = myObj.nextInt();
+
+        //loop until we get valid input
+        while (choice < 1 || choice > 3){
+            System.out.println("Invalid input. Please select a number between 1 and 3 from the following options:");
+            System.out.println("Plaza actions (please enter the # of the action you would like to select:)");
+            System.out.println("1. Visit the crop shop");
+            System.out.println("2. Visit the seed shop");
+            System.out.println("3. Visit the general shop");
+            choice = myObj.nextInt();
+        }
+
+        return choice;
+    }
+
+    public int seedStoreOptions(Player player){
+        //create a Scanner object
+        Scanner myObj = new Scanner(System.in);
+
+        //store whether the user can pay for the item they want
+        boolean cost = true;
+
+        //display the available seed store choices
+        System.out.println("Seeds in stock (please enter the # of the seed you would like to buy:)");
+        System.out.println("1. Carrot");
+        System.out.println("2. Turnip");
+        System.out.println("3. Onion");
+        System.out.println("4. Pumpkin");
+        System.out.println("5. Potato");
+        System.out.println("6. Exit seed shop");
+
+        //store the players choice
+        int choice = myObj.nextInt();
+
+        //check to make sure user has enough money for carrot seeds
+        if (choice == 1){
+            if (player.money - carrot.cost < 0){
+                System.out.println("You do not have enough money to buy carrot seeds.");
+                System.out.println("You have: " + player.money + "g in your pocket.");
+                System.out.println("Carrot seeds cost: " + carrot.cost + "g per packet.");
+                return -1;
+            }
+
+            else{
+                //subtract cost of seeds
+                player.money -= carrot.cost;
+                //increase inventory
+                carrot.inventory += 1;
+            }
+        }
+
+        //check to make sure user has enough money for turnip seeds
+        else if (choice == 2){
+            if (player.money - turnip.cost < 0){
+                System.out.println("You do not have enough money to buy turnip seeds.");
+                System.out.println("You have: " + player.money + "g in your pocket.");
+                System.out.println("Turnip seeds cost: " + turnip.cost + "g per packet.");
+                return -1;
+            }
+
+            else{
+                //subtract cost of seeds
+                player.money -= turnip.cost;
+                //increase inventory
+                turnip.inventory += 1;
+            }
+
+        }
+
+        //check to make sure user has enough money for onion seeds
+        else if (choice == 3){
+            if (player.money - onion.cost < 0){
+                System.out.println("You do not have enough money to buy onion seeds.");
+                System.out.println("You have: " + player.money + "g in your pocket.");
+                System.out.println("Onion seeds cost: " + onion.cost + "g per packet.");
+                return -1;
+            }
+
+            else{
+                //subtract cost of seeds
+                player.money -= onion.cost;
+                //increase inventory
+                onion.inventory += 1;
+            }
+        }
+
+        //check to make sure user has enough money for pumpkin seeds
+        else if (choice == 4){
+            if (player.money - pumpkin.cost < 0){
+                System.out.println("You do not have enough money to buy pumpkin seeds.");
+                System.out.println("You have: " + player.money + "g in your pocket.");
+                System.out.println("Pumpkin seeds cost: " + pumpkin.cost + "g per packet.");
+                return -1;
+            }
+
+            else{
+                //subtract cost of seeds
+                player.money -= pumpkin.cost;
+                //increase inventory
+                pumpkin.inventory += 1;
+            }
+        }
+
+        //check to make sure user has enough money for potato seeds
+        else if (choice == 5){
+            if (player.money - potato.cost < 0){
+                System.out.println("You do not have enough money to buy potato seeds.");
+                System.out.println("You have: " + player.money + "g in your pocket.");
+                System.out.println("Potato seeds cost: " + potato.cost + "g per packet.");
+                return -1;
+            }
+
+            else{
+                //subtract cost of seeds
+                player.money -= potato.cost;
+                //increase inventory
+                potato.inventory += 1;
+            }
+        }
+
+        //loop until we get valid input
+        while (choice < 1 || choice > 6){
+
+            //check if user wants to exit seed shop
+            if (choice == 6){
+                return choice;
+            }
+            
+            System.out.println("Invalid input. Please select a number between 1 and 3 from the following options:");
+            System.out.println("Seeds in stock (please enter the # of the seed you would like to buy:)");
+            System.out.println("1. Carrot");
+            System.out.println("2. Turnip");
+            System.out.println("3. Onion");
+            System.out.println("4. Pumpkin");
+            System.out.println("5. Potato");
+            System.out.println("6. Exit seed shop");
+            choice = myObj.nextInt();
+        }
+        return choice;
+    }
+
+
     public void startADay(Player player){
         //create a variable that keeps track of how many actions player is allowed to do in a day
         int numberOfActions = 3;
@@ -90,13 +258,41 @@ public class Farm {
 
             //player wants to go to plaza
             if (choice == 1){
-                System.out.println("Player wants to go to the plaza");
+                System.out.println("Welcome to the plaza!");
+                int plazaOption = plazaOptions();
+
+                //crop shop 
+                if (plazaOption == 1){
+                    //TODO
+                    System.out.println("Welcome to the crop shop.");
+                }
+
+                //seed shop
+                else if (plazaOption == 2){
+                    System.out.println("Welcome to the seed shop.");
+
+                    //store player option 
+                    int seedStoreOption = 0;
+
+                    //keep going to the seed store until user wants to exit
+                    while (seedStoreOption != 6){
+                        seedStoreOption = seedStoreOptions(player);
+                    }
+
+                }
+
+                //general shop
+                else if (plazaOption == 3){
+                    //TODO
+                    System.out.println("Welcome to the general shop.");
+                }
+
             }
 
             //player wants to tend to their farm
             else if (choice == 2){
                 System.out.println("The mayor has a message for player");
-                mayor.Announcement("Add 10g to your inventory for tending your farm. Every day that you complete farm work, you will earn 10g.")
+                //mayor.Announcement("Add 10g to your inventory for tending your farm. Every day that you complete farm work, you will earn 10g.");
                 System.out.println("Player wants to tend to their farm");
             }
 
---
+++ Project6/src/Seed.java
@@ -0,0 +1,20 @@
+package Project6.src;
+
+public class Seed {
+    String type; //the type of seed
+    double cost; //the cost of the seed
+    double sell_price; //how much the seed sells for after it's harvested
+    int inventory; //how much the player has in their inventory of this type of seed
+    double status; //the status of the seed, the fraction of days it has/needs to grow
+
+    public Seed(String type, double cost, double sell_price, int status){
+        this.type = type;
+        this.cost = cost;
+        this.sell_price = sell_price;
+        this.status = status;
+
+        //set inventory initally to 0
+        inventory = 0;
+    }
+
+}
