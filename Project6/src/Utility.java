8651254aabc1df4375332ea68657ededed7084e6
eljay <64116056+eljay@users.noreply.github.com>
2021-11-17 15:42:23 -0700

    Update Utility.java

--- Project6/src/Utility.java
+++ Project6/src/Utility.java
@@ -8,6 +8,7 @@ import java.util.*;
 
 public interface Utility {
     // https://www.baeldung.com/java-generating-random-numbers-in-range
+    // Referenced this source that was included in Professor Montgomery's implementation
     static int rndFromRange(int min, int max) {
         //returns a uniform inclusive random number from a given min and max range
         return (int) ((Math.random() * ((max+1) - min)) + min);
